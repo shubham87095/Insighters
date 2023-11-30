64bf0de55c53715dab5f03613c5dca0153282f95
shubham87095 <shubhamgla4097@gmail.com>
2023-11-30 21:03:36 +0530

    this is my mini project named "Insighters" which is an E-Learning website
    My mini project

---
+++ Insighters/about.php
@@ -0,0 +1,41 @@
+<h1><?php echo $title;?></h1>
+<p>Welcome to Insighters - Your Gateway to Knowledge</p>
+
+<p>At Insighters, we are passionate about fostering a transformative learning experience. Our platform is designed to empower learners of all backgrounds, ages, and locations to engage with high-quality educational content at their convenience.</p>
+
+<p>Our Vision</p>
+
+<p>We envision a world where learning knows no bounds. Our goal is to break barriers and provide an accessible, flexible, and personalized learning environment for every individual eager to explore new horizons.</p>
+
+<p>Who We Are</p>
+
+<p>We are a team of dedicated educators, technologists, and enthusiasts who believe in the power of knowledge to shape the future. Our diverse backgrounds converge to create a platform that amalgamates expertise, innovation, and user-centric design.</p>
+
+<p>What We Offer</p>
+
+Insighters hosts a comprehensive array of courses spanning various disciplines. From foundational topics to specialized subjects, our platform features engaging content crafted by experts in their fields. Through interactive lessons, quizzes, and projects, we aim to make learning both enjoyable and rewarding.</p>
+
+<p>Why Choose Insighters?</p>
+
+<p> Learn at your own pace, anytime, anywhere.
+Quality Content: Curated courses designed to meet industry standards.
+Personalized Learning: Tailored pathways to suit individual learning styles.
+Community Interaction: Engage with fellow learners, instructors, and experts.
+Progress Tracking: Monitor your growth and achievements effortlessly.
+Join Us on this Journey
+
+Whether you're a student seeking to expand your knowledge, a professional aiming to upskill, or an enthusiast eager to explore new passions, Insighters welcomes you. Together, let's embark on a journey of discovery and continuous learning.</p>
+
+<p>Get in Touch </p>
+
+<p>Have questions or suggestions? We'd love to hear from you! Reach out to our support team or connect with us on social media. Your feedback fuels our commitment to improving your learning experience.
+
+Thank you for choosing Insighters as your learning companion. Let's unlock the doors to knowledge and embark on a journey of endless possibilities!
+
+Feel free to adjust or personalize this content according to the specific mission and goals of your e-learning platform, as well as any unique features or values you want to highlight!
+
+
+
+
+
+</p>
---
+++ Insighters/home.php
@@ -0,0 +1,60 @@
+<!-- <h1><?php echo $title;?></h1> -->
+
+<div class="col-lg-12">  
+<h4>Mandate</h4>
+    <p> At Insighters, we're dedicated to revolutionizing the way individuals learn and grow. Our mandate is rooted in the belief that education should be accessible, engaging, and transformative for everyone, regardless of background or location.</p> 
+</div>
+<div  class="col-lg-6">
+  <h4>Vision</h4> 
+  <p>Center of Academic Excellence Delivering Quality Service to all.</p>
+  <h4>Mission</h4>
+  <p>To democratize education by providing high-quality, affordable, and diverse learning opportunities.</p>
+  <p>To empower learners with the skills, knowledge, and mindset needed to thrive in an ever-evolving world.</p>
+  <p>To foster a community-driven platform that encourages collaboration, curiosity, and continuous learning.
+ </p>
+  <h4>Goals</h4> 
+  <ol>
+    <li>Empowering Learning Communities</li>  
+    <li>Accessible & Diverse Learning Resources</li> 
+    <li>Community Engagement & Support</li> 
+    <li> Enhancing Skills & Knowledge</li>  
+    <li>Effective and efficient administration </li>  
+  </ol>
+
+  <h4>Objectives</h4> 
+   <ol>
+      <li>Create an environment of shared leadership and responsibilities with competent administrator.</li>
+      <li>Provide relevant trainings and seminar to faculty, staff, and student.</li>
+      <li> Foster engaging learning experiences through interactive tools, forums, and collaborations.</li>
+      <li>Conduct relevant and updated researches</li>
+      <li>Extend financial support and manpower for outreach activities.</li> 
+   </ol> 
+</div>
+<div  class="col-lg-6">
+  <h4>Guiding Principles</h4> 
+    <ul>
+      <li>Academic Freedom </li>
+      <li>Responsibility</li>
+      <li> Academic Standards</li>  
+    </ul>
+<h4>Core Values</h4>
+  <ul>
+    <li>God-Centered</li>
+    <li>Excellence</li>
+    <li>Integrity</li>
+    <li>Transparency and Accountability</li>
+    <li>Dedication to Quality Service</li>  
+  </ul> 
+<h4>Quality Policy</h4> 
+<ol>
+    <li>Insighters is committed to be the center of academic excellence delivering quality service to all by;</li> 
+    <li>Continuing innovations and quality improvement cultivating an efficient and effective academic environment for maximum clientele satisfaction;</li> 
+    <li>Adhering to laws and regulations, global standards, and educational requirements;</li> 
+    <li>Participating in sustainable developments projects for inclusive economic for inclusive 
+economic growth;</li> 
+    <li>Showcasing quality outputs; and,</li> 
+    <li>Upholding values and integrity and nurturing talents and skills for global academic competitiveness.</li>  
+</ol>
+</div>
+ 
+
